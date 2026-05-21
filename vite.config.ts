1a51ad6f0a1c50c092d29dd72cf7bc10c490c5b8
1Dr490n <idragon3o66@gmail.com>
2026-05-21 18:49:26 +0200

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		https: {
+			key: fs.readFileSync(`/etc/letsencrypt/live/mango.thein.nl/privkey.pem`),
+			cert: fs.readFileSync(`/etc/letsencrypt/live/mango.thein.nl/cert.pem`)
+		},
+		proxy: {}
+	}
 });
