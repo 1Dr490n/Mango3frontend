cc74e3e153e5abfa7ba0ea5e83248d6bcdfd9f6c
1Dr490n <idragon3o66@gmail.com>
2026-06-11 14:34:03 +0200

    a

--- src/routes/group/[id]/+page.svelte
+++ src/routes/group/[id]/+page.svelte
@@ -191,7 +191,22 @@
 						}}
 					/>
 				</div>
-				<input type="submit" class="bg-fg mb-1 w-30 rounded-md p-2 ml-1" />
+				<button class="bg-fg ml-1 rounded-md p-2 mb-1"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+						/>
+					</svg></button
+				>
 				<select class="mb-1 ml-1 w-30 rounded-md bg-fg p-1" name="type">
 					<option>Song</option>
 					<option>Album</option>
